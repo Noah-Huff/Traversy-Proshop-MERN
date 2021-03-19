61b53c04cb36730bf38e82b94db78e0d0258cdf5
Noah-Huff <noah.b.huff@gmail.com>
2021-03-19 08:34:03 -0500

    creating a simple backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
 //import logo from './logo.svg';
 //import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Homescreen from './screens/Homescreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-    <h1>Welcome To Pro Shop</h1>
-      
-    </>
+    <Router>
+      <Header/>
+      <Container>
+        <main className="py-3">  
+          <Route path='/' component={Homescreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </main>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
