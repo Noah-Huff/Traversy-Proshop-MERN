c42aad48bab7f32fe03bb1438fabf4424bb1a718
Noah-Huff <>
2021-04-19 14:18:18 -0500

    home and product page redux

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,32 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+app.use('/api/products', productRoutes);
 
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/product/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, console.log('Server running on port 5000'));
+app.listen(
+    PORT, 
+    console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+        )
+    );
